153fa41879a46848f47d5a918d2dd6b573ae1d27
prathamtangri1 <prathamtangri2015@gmail.com>
2020-10-21 08:22:57 +0100

    removed useless api requests from frontend

--- Frontend/material-dashboard-react-master/src/views/Account/AccountDetails.js
+++ Frontend/material-dashboard-react-master/src/views/Account/AccountDetails.js
@@ -99,28 +99,10 @@ export default function AccountDetails() {
                 <div style={{clear: "both"}}></div>
             </div>
 
-            <p>Here you can make all of the following API requests:</p>
+            <p>Here you can find the following balance information</p>
 
             <div class="box">
-                <h3 class="box__heading">Products</h3>
-
-                {/* <!-- Auth --> */}
-                <div class="item-data-row">
-                <div class="item-data-row__left">
-                    <div class="item-data-row__request-type">post</div>
-                </div>
-                <div class="item-data-row__center">
-                    <div class="item-data-row__nicename">Auth</div>
-                    <div class="item-data-row__endpoint">/auth/get</div>
-                    <div class="item-data-row__description">Retrieve account and routing numbers for checking and savings accounts.</div>
-                </div>
-                <div class="item-data-row__right">
-                    <button id="get-auth-btn" class="button button--is-small button--is-default button--is-full-width">Send request</button>
-                </div>
-                <div class="item-data-row__response">
-                    <table><tbody id="get-auth-data"></tbody></table>
-                </div>
-                </div>
+                {/* <h3 class="box__heading">Products</h3> */}
 
                 {/* <!--Transactions --> */}
                 <div class="item-data-row">
@@ -140,24 +122,6 @@ export default function AccountDetails() {
                 </div>
                 </div>
 
-                {/* <!-- Identity --> */}
-                <div class="item-data-row">
-                <div class="item-data-row__left">
-                    <div class="item-data-row__request-type">post</div>
-                </div>
-                <div class="item-data-row__center">
-                    <div class="item-data-row__nicename">Identity</div>
-                    <div class="item-data-row__endpoint">/identity/get</div>
-                    <div class="item-data-row__description">Retrieve Identity information on file with the bank. Reduce fraud by comparing user-submitted data to validate identity.</div>
-                </div>
-                <div class="item-data-row__right">
-                    <button id="get-identity-btn" class="button button--is-small button--is-default button--is-full-width">Send request</button>
-                </div>
-                <div class="item-data-row__response">
-                    <table><tbody id="get-identity-data"></tbody></table>
-                </div>
-                </div>
-
                 {/* <!-- Balance --> */}
                 <div class="item-data-row">
                 <div class="item-data-row__left">
@@ -173,46 +137,6 @@ export default function AccountDetails() {
                 </div>
                 <div class="item-data-row__response">
                     <table><tbody id="get-balance-data"></tbody></table>
-                </div>
-                </div>
-
-                {/* <!-- Assets (hidden unless 'assets' is included in the product list) --> */}
-                <div id='assets' class="item-data-row" style={{display:"none"}}>
-                <div class="item-data-row__left">
-                    <div class="item-data-row__request-type">post</div>
-                </div>
-                <div class="item-data-row__center">
-                    <div class="item-data-row__nicename">Assets</div>
-                    <div class="item-data-row__endpoint">/asset_report/*</div>
-                    <div class="item-data-row__description">Create a point-in-time snapshot of a user's assets.</div>
-                </div>
-
-                <div class="item-data-row__right">
-                    <button id="get-assets-btn" class="button button--is-small button--is-default button--is-full-width">Send request</button>
-
-                    <a id="download-assets-pdf-btn" class="button button--is-small button--is-primary button--is-full-width" style={{display: "none", margin: "10px 0px"}}>Download as PDF</a>
-                </div>
-
-                <div class="item-data-row__response">
-                    <table><tbody id="get-assets-data"></tbody></table>
-                </div>
-                </div>
-
-                {/* <!-- Holdings --> */}
-                <div class="item-data-row">
-                <div class="item-data-row__left">
-                    <div class="item-data-row__request-type">post</div>
-                </div>
-                <div class="item-data-row__center">
-                    <div class="item-data-row__nicename">Holdings</div>
-                    <div class="item-data-row__endpoint">/investments/holdings/get</div>
-                    <div class="item-data-row__description">Retrieve investment holdings on file with the bank, brokerage, or investment institution. Analyze over-exposure to market segments.</div>
-                </div>
-                <div class="item-data-row__right">
-                    <button id="get-holdings-btn" class="button button--is-small button--is-default button--is-full-width">Send request</button>
-                </div>
-                <div class="item-data-row__response">
-                    <table><tbody id="get-holdings-data"></tbody></table>
                 </div>
                 </div>
 
@@ -254,46 +178,6 @@ export default function AccountDetails() {
                 </div>
                 </div>
 
-            </div>
-
-            <div class="box">
-                <h3 class="box__heading">Item management</h3>
-
-                <div class="item-data-row">
-                <div class="item-data-row__left">
-                    <div class="item-data-row__request-type">post</div>
-                </div>
-                <div class="item-data-row__center">
-                    <div class="item-data-row__endpoint">/item/get</div>
-                    <div class="item-data-row__description">Retrieve information about an Item, like the institution, billed products, available products, and webhook information.</div>
-                </div>
-
-                <div class="item-data-row__right">
-                    <button id="get-item-btn" class="button button--is-small button--is-default button--is-full-width">Send request</button>
-                </div>
-
-                <div class="item-data-row__response">
-                    <table><tbody id="get-item-data"></tbody></table>
-                </div>
-                </div>
-
-                <div class="item-data-row">
-                <div class="item-data-row__left">
-                    <div class="item-data-row__request-type">post</div>
-                </div>
-                <div class="item-data-row__center">
-                    <div class="item-data-row__endpoint">/accounts/get</div>
-                    <div class="item-data-row__description">Retrieve high-level information about all accounts associated with an Item.</div>
-                </div>
-
-                <div class="item-data-row__right">
-                    <button id="get-accounts-btn" class="button button--is-small button--is-default button--is-full-width">Send request</button>
-                </div>
-
-                <div class="item-data-row__response">
-                    <table><tbody id="get-accounts-data"></tbody></table>
-                </div>
-                </div>
             </div>
         </div>
     );
